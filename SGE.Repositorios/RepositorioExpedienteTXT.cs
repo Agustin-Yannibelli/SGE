335439a1891adac1d02a725af0894532cbc5422e
Agustin <agustin.yannibelli@gmail.com>
2024-05-04 23:18:29 -0300

    se codifico el AltaExpediente incluyendo los permisos

--- SGE.Repositorios/RepositorioExpedienteTXT.cs
+++ SGE.Repositorios/RepositorioExpedienteTXT.cs
@@ -3,5 +3,26 @@ using SGE.Aplicacion;
 
 public class RepositorioExpedienteTXT : IExpedienteRepositorio
 {
+  readonly string _nombreArch = @"C:\Users\agust\OneDrive\Escritorio\proyectoExpedientes\SGE\SGE.Repositorios\Expedientes.txt";
 
+  public void AltaExpediente(Expediente expediente, int IdUser)
+  {
+      ServicioAutorizacionProvisorio permiso = new ServicioAutorizacionProvisorio();
+      
+    if(!permiso.PoseeElPermiso(IdUser, Permiso.AltaExpediente))
+    {
+    Console.WriteLine("El usuario no tiene permiso para dicha accion");
+    return;
+    }
+  
+    using (var sw = new StreamWriter(_nombreArch, true))
+    {
+      sw.WriteLine(expediente.IdTramite);
+      sw.WriteLine(expediente.Caratula);
+      sw.WriteLine(expediente.FechaYHoraCreacion);
+      sw.WriteLine(expediente.FechaYHoraUltModificacion);
+      sw.WriteLine(expediente.UsuarioUltModificacion);
+      sw.WriteLine(expediente.Estado);
+    }
+  }  
 }
