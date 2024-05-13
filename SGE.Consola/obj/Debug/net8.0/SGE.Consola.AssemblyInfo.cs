3896810acc8be3d8a778ebe3317b06fcdd013984
Agustin <agustin.yannibelli@gmail.com>
2024-05-13 01:24:57 -0300

    nada

--- SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc9c26a64a50cda0e9e09f9613f3efea3bcd517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba8d134ef786fd1649e91843b822958e2f1ccda3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
